2eae570ea75499008cb00064d0102193aff1c913
minli <minli@thoughtworks.com>
2013-06-23 17:56:37 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'e1a96ed7254b689ec8b218c60f157b9273981d988c0174a746220b5b41fdff32d541188056149de0ebcd5cf597b4bd6f1a45c421b37dddc039a7fa6a84018f53'
